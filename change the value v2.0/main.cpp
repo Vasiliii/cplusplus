bb251a294c66b62fea15205377bd325314a1cd6d
Vasiliii <60130126+Vasiliii@users.noreply.github.com>
2023-04-26 21:12:02 +0300

    Add files via upload

---
+++ change the value v2.0/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+
+//declaration of function
+void chagerValues(T &a, T &b)
+{
+
+    T temp = a;
+    a = b;
+    b = temp;
+
+}
+
+int main()
+{
+    string a;
+    string b;
+
+    cout << "Enter the first argument: ";
+    cin >> a;
+
+    cout << "Enter the second argument: ";
+    cin >> b;
+
+    cout << "\na = " << a <<endl;
+    cout << "b = " << b <<endl;
+
+    cout << "\n===================\nchange\n" <<endl;
+
+    chagerValues(a, b);
+    cout << "a = " << a <<endl;
+    cout << "b = " << b <<endl;
+
+    return 0;
+}
